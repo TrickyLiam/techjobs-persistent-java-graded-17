4dbe1546d913413ce8faf41e23c239978a16f470
TrickyLiam <liamcarlson2014@gmail.com>
2023-10-30 20:54:13 -0500

    finish task 2

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -2,20 +2,33 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+    @GetMapping("/")
+    public String index (Model model){
+        List employer = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employer);
+        model.addAttribute("title", "All Employers");
+        return "employers/index";
+    }
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
+        model.addAttribute("employer", "Employer Category");
         model.addAttribute(new Employer());
         return "employers/add";
     }
@@ -25,16 +38,18 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("employer", new Employer());
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
@@ -42,6 +57,13 @@ public class EmployerController {
         } else {
             return "redirect:../";
         }
+    }
 
+    @PostMapping("employers")
+    public String displayAllEmployers(Model model){
+        List employer = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employer);
+        model.addAttribute("title", "All Employers");
+        return "employers/index";
     }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 300, message = "Description can be no longer than 300 characters")
+    private String description;
+
+    public Skill (){}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
